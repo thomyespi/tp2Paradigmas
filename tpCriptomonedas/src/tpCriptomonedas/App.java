47fa789cd06488ef6b68ff4b5c95f23cda205b42
tespinosa <tespinosa@AP-ARBAS-L1854.accionpoint.local>
2024-06-02 17:38:49 -0300

    segundos cambios 02/06

---
+++ tpCriptomonedas/src/tpCriptomonedas/App.java
@@ -0,0 +1,14 @@
+package tpCriptomonedas;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		GestorSistema sistema = new GestorSistema();
+		
+		sistema.mostrarMenu();
+		
+		System.out.println("\\nEl programa finalizo bien. Gracias por participar");
+	}
+
+}
--- tpCriptomonedas/src/tpCriptomonedas/GestorSistema.java
+++ tpCriptomonedas/src/tpCriptomonedas/GestorSistema.java
@@ -1,6 +1,65 @@
 package tpCriptomonedas;
 
+import java.util.Scanner;
+
 public class GestorSistema {
 
-	
+	private GestorCripto gestor;
+
+	public GestorSistema() {
+
+		this.gestor = new GestorCripto();
+	}
+
+	public void mostrarMenu() {
+
+		final Scanner scanner = new Scanner(System.in);
+		int opcion;
+
+		do {
+
+			System.out.println("================== MENU =================");
+			System.out.println("=========================================\n");
+
+			System.out.println("[1] Mostrar todas las Criptomonedas");
+			System.out.println("[2] Mostrar todos los Mercados");
+			System.out.println("[3] Agregar Criptomoneda");
+			System.out.println("[4] Modificar Criptomoneda");
+			System.out.println("[5] Eliminar Criptomoneda");
+			System.out.println("[0] Finalizar Programa");
+			System.out.println("\nSeleccionar Opcion: ");
+			try {
+				opcion = Integer.parseInt(scanner.nextLine());
+			} catch (Exception e) {
+				opcion = -1;
+			}finally {
+				scanner.close();
+			}
+
+			switch (opcion) {
+			case 1:
+				 this.gestor.getCriptomonedas();
+				break;
+			case 2:
+				this.gestor.getMercados();
+				break;
+			case 3:
+				// this.addCryptoMenu();
+				break;
+			case 4:
+				// this.updateCryptoMenu();
+				break;
+			case 5:
+				// this.removeCryptoMenu();
+				break;
+			case 0:
+				System.out.println("Terminando...");
+				break;
+			default:
+				System.out.println("La opcion elegida no es Valida!!\n");
+			}
+
+		} while (opcion != 0);
+
+	}
 }
